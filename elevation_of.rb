4d89465e03f46331bcda0774861ee5375b25fe41
Hidenori FUJIMURA <hfu@solid-state-survivor.local>
2015-01-05 00:10:20 +0900

    update to get elevation from tiles and use cache

--- elevation_of.rb
+++ elevation_of.rb
@@ -1,8 +1,36 @@
 require 'json'
 require 'open-uri'
 
+#def elevation_of(lng, lat)
+#  e = JSON.parse(open("http://cyberjapandata2.gsi.go.jp/general/dem/scripts/getelevation.php?lon=#{lng}&lat=#{lat}&outtype=JSON").read)['elevation']
+#  raise 'elevation not found' if e === '-----'
+#  e
+#end
+
+module Math
+  def self.sec(x)
+    1.0 / cos(x)
+  end
+end
+
+def lnglat2xy(lng, lat, z)
+  n = 2 ** z
+  rad = lat * 2 * Math::PI / 360
+  [n * ((lng.to_f + 180) / 360),
+   n * (1 - (Math::log(Math::tan(rad) +
+   Math::sec(rad)) / Math::PI)) / 2]
+end
+
+$buf = {}
 def elevation_of(lng, lat)
-  e = JSON.parse(open("http://cyberjapandata2.gsi.go.jp/general/dem/scripts/getelevation.php?lon=#{lng}&lat=#{lat}&outtype=JSON").read)['elevation']
-  raise 'elevation not found' if e === '-----'
-  e
+  (xf, yf) = lnglat2xy(lng, lat, 14)
+  xy = "#{xf.to_i}/#{yf.to_i}"
+  url = "http://cyberjapandata.gsi.go.jp/xyz/dem/14/#{xy}.txt"
+  if $buf[xy].nil?
+    $buf[xy] = []
+    open(url).each_line {|l|
+      $buf[xy] << l.strip.split(',').map{|v| v.to_f}
+    }
+  end
+  $buf[xy][(yf % 1 * 256).to_i][(xf % 1 * 256).to_i]
 end
--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
 require './elevation_of.rb'
-
-p elevation_of(35, 35)
+p elevation_of(135, 35)
+p elevation_of(135, 35)
+p elevation_of(135, 35)
+p elevation_of(135, 35.1)
